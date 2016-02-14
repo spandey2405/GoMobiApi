712ce8a97d28141678eb78bd26891a71ad9f2cd6
Saurabh Pandey <spandey2405@gmail.com>
2016-02-14 17:51:01 +0530

    Started Working On Api

--- urls.py
+++ urls.py
@@ -7,8 +7,8 @@ from src.api.v1.views.brandview import BrandView
 
 
 urlpatterns = [
-     url(r'^admin/v1/user/$', UserView.as_view()),
-     url(r'^admin/v1/login/$', TokenView.as_view()),
-     url(r'^gomobi/v1/brands/$', BrandView.as_view()),
+     url(r'^gomobi/admin/v1/user/$', UserView.as_view()),
+     url(r'^gomobi/admin/v1/login/$', TokenView.as_view()),
+     url(r'^gomobi/web/v1/brands/$', BrandView.as_view()),
      ]
 
