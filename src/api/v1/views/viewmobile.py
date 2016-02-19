cc3967033161765154d2bf5446d73b68a73fbd38
Saurabh Pandey <spandey2405@gmail.com>
2016-02-20 02:33:33 +0530

    Added Files

--- src/api/v1/views/viewmobile.py
+++ src/api/v1/views/viewmobile.py
@@ -18,7 +18,7 @@ api_lib = MobileLib()
 }
 '''
 
-class MobilesView( LoggingMixin, generics.GenericAPIView, mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin):
+class MobileView( LoggingMixin, generics.GenericAPIView, mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin):
     model = Mobiles
     serializer_class = BrandSerializer
     # permission_classes = (permissions.IsAuthenticated, )
--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ from src.api.v1.views.userview import UserView
 from src.api.v1.views.tokenview import TokenView
 from src.api.v1.views.brandview import BrandView
 from src.api.v1.views.mobileview import MobilesView
+from src.api.v1.views.viewmobile import MobileView
 
 
 urlpatterns = [
@@ -12,6 +13,6 @@ urlpatterns = [
      url(r'^gomobi/admin/v1/login/$', TokenView.as_view()),
      url(r'^gomobi/web/v1/brands/$', BrandView.as_view()),
      url(r'^gomobi/web/v1/mobiles/$', MobilesView.as_view()),
-     url(r'^gomobi/web/v1/mobile/$', MobilesView.as_view()),
+     url(r'^gomobi/web/v1/mobile/$', MobileView.as_view()),
      ]
 
