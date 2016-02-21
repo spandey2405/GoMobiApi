a642ea65f381272810164ef68bfaa8734bd771c8
Saurabh Pandey <spandey2405@gmail.com>
2016-02-21 20:37:25 +0530

    Added Mobiles View

--- src/common/libraries/mobiles/mobilelib.py
+++ src/common/libraries/mobiles/mobilelib.py
@@ -7,29 +7,37 @@ class MobileLib():
 
     def get_mobiles(self, queryobj):
         page = 1
+
         if "page" in queryobj:
             page = int(queryobj['page'])
 
         startlist = (32 * (page - 1 ))
 
-        if "u" in queryobj:
-            searchfor = queryobj['u'].lower()
-            total = Mobiles.objects.filter(name__like = searchfor).count()
-            Query = "SELECT * FROM `gomobi_mobiles` WHERE `name` LIKE '%{0}%' ORDER BY `gomobi_mobiles`.`mobile_id` DESC LIMIT {1} , 32".format(str(searchfor),str(startlist))
-            mobiles = Mobiles.objects.raw(Query)
-        else :
-            total = Mobiles.objects.all().count()
-            Query = "SELECT * FROM `gomobi_mobiles` ORDER BY `gomobi_mobiles`.`mobile_id` DESC LIMIT {0} , 32".format(str(startlist))
-            mobiles = Mobiles.objects.raw(Query)
+        try:
 
-        response = OrderedDict()
-        response['total'] = total
-        response['data'] = OrderedDict()
-        for mobile in mobiles:
-            selected = MobileSerializer(mobile).data
-            response['data'][selected[KEY_MOBILE_ID]] = selected
+            if "u" in queryobj:
+                searchfor = queryobj['u'].lower()
+                total = Mobiles.objects.filter(name__like = searchfor).count()
+                Query = "SELECT * FROM `gomobi_mobiles` WHERE `name` LIKE '%{0}%' ORDER BY `gomobi_mobiles`.`mobile_id` DESC LIMIT {1} , 32".format(str(searchfor),str(startlist))
+                mobiles = Mobiles.objects.raw(Query)
+            else :
+                total = Mobiles.objects.all().count()
+                Query = "SELECT * FROM `gomobi_mobiles` ORDER BY `gomobi_mobiles`.`mobile_id` DESC LIMIT {0} , 32".format(str(startlist))
+                mobiles = Mobiles.objects.raw(Query)
 
-        return response
+            response = OrderedDict()
+
+            response['total'] = total
+            response['data'] = OrderedDict()
+
+            for mobile in mobiles:
+                selected = MobileSerializer(mobile).data
+                response['data'][selected[KEY_MOBILE_ID]] = selected
+    
+            return response
+
+        except Exception as e:
+            print e
 
     def add_mobiles(self, mobilelist):
 
